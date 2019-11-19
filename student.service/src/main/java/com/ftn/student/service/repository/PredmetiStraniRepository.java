d40079b8ade2987e2a9e7f1db9bdf41757d8f60f
mile <mile.miljanovic@devoteam.com>
2019-11-19 16:53:16 +0100

    Manja korekcija u repozitorijumu

--- student.service/src/main/java/com/ftn/student/service/repository/PredmetiStraniRepository.java
+++ student.service/src/main/java/com/ftn/student/service/repository/PredmetiStraniRepository.java
@@ -3,10 +3,10 @@ package com.ftn.student.service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ftn.student.service.models.PredmetDomaciID;
 import com.ftn.student.service.models.PredmetStrani;
+import com.ftn.student.service.models.PredmetStraniID;
 
 @Repository
-public interface PredmetiStraniRepository extends JpaRepository<PredmetStrani, PredmetDomaciID> {
+public interface PredmetiStraniRepository extends JpaRepository<PredmetStrani, PredmetStraniID> {
 
 }
