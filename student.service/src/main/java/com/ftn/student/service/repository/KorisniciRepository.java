d6f8409219cf2662b882efe91d7d179bdabea910
mile <mile.miljanovic@devoteam.com>
2019-12-12 16:07:20 +0100

    Dodat query za korisnike

--- student.service/src/main/java/com/ftn/student/service/repository/KorisniciRepository.java
+++ student.service/src/main/java/com/ftn/student/service/repository/KorisniciRepository.java
@@ -1,11 +1,19 @@
 package com.ftn.student.service.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ftn.student.service.models.Korisnik;
+import com.ftn.student.service.models.Uloga;
 
 @Repository
 public interface KorisniciRepository extends JpaRepository<Korisnik, String> {
+	
+	@Query("select k from korisnici k where k.uloga = :uloga")
+	List<Korisnik> findByRole(@Param("uloga") Uloga uloga);
 
 }
